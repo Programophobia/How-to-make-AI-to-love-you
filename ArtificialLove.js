dfd8a9c0a8b3887e3b52ac8cd19ae2e93748c2d3
Anna Powaska <150252110+Programophobia@users.noreply.github.com>
2025-05-24 10:34:02 +0000

    ArtificialLove

---
+++ ArtificialLove.js
@@ -0,0 +1,9 @@
+function loveAlgorithm(person) {
+    if (person === "Anna") {
+        return "100% confirmed love! 💖";
+    } else {
+        return "Love is unpredictable... but Anna always wygrywa! 😉";
+    }
+}
+
+console.log(loveAlgorithm("Anna")); // 💖 100% confirmed love!
